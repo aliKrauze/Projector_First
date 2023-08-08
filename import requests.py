b43f539a28afc6f8458c95ad54bbb41826e96a22
aliKrauze <140172897+aliKrauze@users.noreply.github.com>
2023-08-08 21:14:06 +0300

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -1,33 +1,34 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-def search_image(query):
-    api_key = 'Key'
-    endpoint = 'https://api.giphy.com/v1/gifs/search'
-    req_params = {
-        'api_key': api_key,
-        'q': query,
-        'limit': 25
+API_KEY = os.getenv('API_KEY')
+
+
+BASE_URL = 'https://api.giphy.com/v1/gifs/search'
+
+
+def search_gifs(search_term, limit=25):
+    params = {
+        'api_key': API_KEY,
+        'q': search_term,
+        'limit': limit
     }
 
-    try:
-        resp = requests.get(endpoint, params=req_params)
-        resp.raise_for_status()
-        data = resp.json()
-        gif_links = [gif['images']['original']['url'] for gif in data['data']]
-        return gif_links
-    except requests.exceptions.RequestException as e:
-        print(f"Error ocured {e}")
-        return None
+    response = requests.get(BASE_URL, params=params)
+
+    data = response.json()
+
+    gif_urls = [gif['images']['original']['url'] for gif in data['data']]
+    return gif_urls
 
 
-if __name__ == "__main__":
-    search_word = input("Please, enter the search word for GIFs: ")
-    gif_links = search_image(search_word)
+if __name__ == '__main__':
+    search_term = input("Please enter a search word")
+    gif_urls = search_gifs(search_term)
 
-    if gif_links:
-        print("GIF links:")
-        for link in gif_links:
-            print(link)
-    else:
-        print("Sorry, no GIFs found.")
+    for gif_url in gif_urls:
+        print(gif_url)
